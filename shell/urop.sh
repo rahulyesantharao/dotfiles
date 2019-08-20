54e078caaf1f53bb06292a8f140695645626b074
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2019-08-20 15:59:30 -0400

    urop config

---
+++ shell/urop.sh
@@ -0,0 +1,6 @@
+export CXX_FLAGS="-Wall -pedantic -Werror -Wno-variadic-macros -Wno-long-long -Wno-shadow -fPIC"
+export CC=gcc-7
+export CXX=g++-7
+export CLASSPATH=".:~/.local/lib/antlr-4.7.2-complete.jar:$CLASSPATH"
+alias antlr='java -jar ~/.local/lib/antlr-4.7.2-complete.jar'
+alias grun='java org.antlr.v4.gui.TestRig'
